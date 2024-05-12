fbb4da70bb2843d9a9dfa8ac24f78b61f2acb783
Vincenzo Saccone <143630974+VinsWhite@users.noreply.github.com>
2024-05-12 17:36:22 +0200

    update

--- PetCenter/frontend/petcenter-react/src/components/create/ValidateComp.tsx
+++ PetCenter/frontend/petcenter-react/src/components/create/ValidateComp.tsx
@@ -1,42 +1,64 @@
 import { Form, Row, Col, Button } from "react-bootstrap";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { scrollToTop } from "../../assets/functions/scrollToTop";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Toast } from 'primereact/toast';
 
 export default function ValidateComp() {
 
     const [validated, setValidated] = useState(false);
+    const address: string = 'http://localhost:3000/pet';
+    const [name, setName] = useState<string>('');
+    const [description, setDescription] = useState<string>('');
+    const [species, setSpecies] = useState<string>('');
+    const navigate = useNavigate();
+    const toast = useRef<Toast>(null);
 
-
-
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-        }
-
-        setValidated(true);
-    };
-
-    // to scroll to top
     useEffect(() => {
 
-        async function fetchData () {
-            try {
-                
-            } catch (error) {
-                console.error(error)
-            }
+        // to scroll to top
+        scrollToTop();
+    }, [])
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const form = event.currentTarget;
+
+        if (form.checkValidity() === false) {
+            event.stopPropagation();
+            setValidated(true);
+            return;
         }
 
-      scrollToTop();
-      fetchData();
-    }, [])
+        try {
+            const response = await axios.post(address, {
+                name: name,
+                description: description,
+                species: species
+            });
+
+            toast.current?.show({ severity: 'info', summary: 'Created', detail: 'Pet created successfully', life: 3000 });
+            console.log(response.data);
+            setTimeout(() => {
+                navigate('/');
+            }, 2000);
+            
+        } catch (error) {
+            console.error(error);
+            toast.current?.show({ severity: 'warn', summary: 'Rejected', detail: 'Action rejected', life: 3000 });
+        }
+
+        setValidated(false);
+        setName('');
+        setDescription('');
+        setSpecies('');
+    }
     
 
   return (
     <>
-        <h2 className="mt-4">Set information about a new Pet!</h2>
+        <h2 className="mt-4">Set informations about a new Pet!</h2>
         <Form noValidate validated={validated} onSubmit={handleSubmit} className="my-5 p-2 border border-2 border-warning rounded-2 shadow">
             <Row className="mb-3">
                 <Form.Group as={Col} md="4" controlId="validationCustom01">
@@ -45,7 +67,8 @@ export default function ValidateComp() {
                             required
                             type="text"
                             placeholder="Enter the name..."
-                            defaultValue="John"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
                         />
                     <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
                 </Form.Group>
@@ -56,7 +79,8 @@ export default function ValidateComp() {
                             required
                             type="text"
                             placeholder="Enter the description..."
-                            defaultValue="He's so cutieee"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
                         />
                     <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
                 </Form.Group>
@@ -67,14 +91,16 @@ export default function ValidateComp() {
                             required
                             type="text"
                             placeholder="Enter the species..."
-                            defaultValue="Cat"
+                            value={species}
+                            onChange={(e) => setSpecies(e.target.value)}
                         />
                     <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
                 </Form.Group>
                 
             </Row>
-            <Button variant="warning" type="submit">Create</Button>
+            <Button className="wiggle" variant="warning" type="submit">Create</Button>
         </Form>
+        <Toast ref={toast} />
     </>
   )
 }
